137da7584af64b6c7313c5b731d531ce48acf463
RaiM593 <rai.mosquera.diaz@utelvt.edu.ec>
2026-04-16 21:32:17 -0500

    Delete eysphpibm directory

--- eysphpibm/public/js/script.js
+++
@@ -1,9 +0,0 @@
-// Script básico para validación de formularios
-function validateForm() {
-    var nombre = document.getElementById("nombre").value;
-    if (nombre == "") {
-        alert("El campo Nombre no puede estar vacío.");
-        return false;
-    }
-    return true;
-}
